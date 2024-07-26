06c73b64bc625a40e950dc816f0b8d21e0214000
Dorian JAMES <dorianthelycan@gmail.com>
2024-07-26 17:54:30 +0300

    UI improvements done on name creation and menu back button and their actions.

--- Assets/Scripts/UI/MainMenu.cs
+++ Assets/Scripts/UI/MainMenu.cs
@@ -1,11 +1,20 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using TMPro;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class MainMenu : MonoBehaviour
 {
     [SerializeField] private TMP_InputField joinCodeField;
+    [SerializeField] private TMP_Text playerPrefsName;
+
+    private void OnEnable()
+    {
+        playerPrefsName.text = PlayerPrefs.GetString("PlayerKey", string.Empty);
+    }
+
     public async void StartHost()
     {
         await HostSingleton.Instance.GameManager.StartHostAsync();
@@ -14,5 +23,11 @@ public class MainMenu : MonoBehaviour
     public async void StartClient()
     {
         await ClientSingleton.Instance.GameManager.StartClientAsync(joinCodeField.text);
+    }
+
+    public void BackAction()
+    {
+        PlayerPrefs.DeleteAll();
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex - 2);
     }
 }
---
+++ Assets/Scripts/UI/NameSelector.cs
@@ -0,0 +1,41 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class NameSelector : MonoBehaviour
+{
+    [SerializeField] private TMP_InputField nameField;
+    [SerializeField] private Button connectButton;
+    [SerializeField] private TMP_Text nameDisplay;
+    [SerializeField] private int minNameLength = 1;
+    [SerializeField] private int maxNameLength = 25;
+
+    private const string PlayerNameKey = "PlayerKey";
+    private void Start()
+    {
+        if (SystemInfo.graphicsDeviceType == UnityEngine.Rendering.GraphicsDeviceType.Null)
+        {
+            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+            return;
+        }
+        nameField.text = PlayerPrefs.GetString(PlayerNameKey, string.Empty);
+        HandleNameChanged();
+    }
+
+    public void HandleNameChanged()
+    {
+        connectButton.interactable = nameField.text.Length >= minNameLength && nameField.text.Length <= maxNameLength;
+        nameDisplay.text = nameField.text;
+    }
+
+    public void Connect()
+    {
+        PlayerPrefs.SetString(PlayerNameKey, nameDisplay.text);
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+        Debug.Log(PlayerPrefs.GetString(nameDisplay.text));
+    }
+}
